1da0d00e60262fc1fa63bbfcd15fde4bcd5de498
David Siher <davidsiher@microsoft.com>
2025-06-23 18:22:33 -0700

    More folder restructuring

--- lib/vagrant/scp-macos.rb
+++
@@ -1,2 +0,0 @@
-require "vagrant/scp-macos/version"
-require "vagrant/scp-macos/plugin"
--- publish_gem.sh
+++ publish_gem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm *.gem
-vim lib/vagrant/scp-macos/version.rb
+vim lib/vagrant/scp/macos/version.rb
 gem build vagrant-scp.gemspec
 gem push vagrant-scp-*.gem
